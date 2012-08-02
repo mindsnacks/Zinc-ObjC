e25c4a8888b193e9f611f19ba58ac73db178b140
Andy Mroczkowski <andy@mrox.net>
2012-08-01 18:04:08 -0700

    Removing KSJSON

--- Zinc/ZincJSONSerialization.m
+++ Zinc/ZincJSONSerialization.m
@@ -7,41 +7,17 @@
 //
 
 #import "ZincJSONSerialization.h"
-#import "ZincKSJSON.h"
 
 @implementation ZincJSONSerialization
 
-static id _NSJSONSerialization = nil;
-
-+ (void)initialize
-{
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        Class cls = NSClassFromString(@"NSJSONSerialization");
-        if (cls != nil) {
-            _NSJSONSerialization = cls;
-        }
-    });
-}
-
 + (id)JSONObjectWithData:(NSData *)data options:(NSJSONReadingOptions)opt error:(NSError **)error
 {
-    if (_NSJSONSerialization != nil) {
-        return [_NSJSONSerialization JSONObjectWithData:data options:opt error:error];
-    } else {
-        NSString *jsonString = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
-        return [ZincKSJSON deserializeString:jsonString error:error];
-    }
+    return [NSJSONSerialization JSONObjectWithData:data options:opt error:error];
 }
 
 + (NSData *)dataWithJSONObject:(id)obj options:(NSJSONWritingOptions)opt error:(NSError **)error
 {
-    if (_NSJSONSerialization != nil) {
-        return [_NSJSONSerialization dataWithJSONObject:obj options:opt error:error];
-    } else {
-        NSString *jsonString = [ZincKSJSON serializeObject:obj error:error];
-        return [jsonString dataUsingEncoding:NSUTF8StringEncoding];
-    }
+    return [NSJSONSerialization dataWithJSONObject:obj options:opt error:error];
 }
 
 
