7cfff5f0802d39ee863db25b211c773ebcbb243d
Andy Mroczkowski <andy@mrox.net>
2012-08-16 09:38:14 -0700

    Removing commented out method

--- Zinc/ZincRepo.h
+++ Zinc/ZincRepo.h
@@ -111,8 +111,6 @@ extern NSString* const ZincRepoBundleCloneProgressKey;
 
 - (ZincBundle*) bundleWithId:(NSString*)bundleId;
 
-//- (void) setPriority:(NSOperationQueuePriority)priority forBundleWithId:(NSString*)bundleId;
-
 // NOTE: this may be removed soon
 - (void) waitForAllBootstrapTasks;
 
