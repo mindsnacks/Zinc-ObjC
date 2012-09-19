dfee54abf2c6eb3be6d159ac8e85aff09f35da0f
Andy Mroczkowski <andy@mrox.net>
2012-09-19 18:02:12 -0400

    Including an updated private AFNetworking
    This is not ideal, but its a short term solution until real dependenct management is figured out.

--- Dependencies/AFNetworking+Zinc/ZincHTTPRequestOperation.h
+++ Dependencies/AFNetworking+Zinc/ZincHTTPRequestOperation.h
@@ -1,4 +1,4 @@
-// AFHTTPOperation.h
+// AFHTTPRequestOperation.h
 //
 // Copyright (c) 2011 Gowalla (http://gowalla.com/)
 // 
@@ -21,60 +21,25 @@
 // THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
-#import "ZincNetworkOperation.h"
+#import "ZincURLConnectionOperation.h"
 
 /**
  `AFHTTPRequestOperation` is a subclass of `AFURLConnectionOperation` for requests using the HTTP or HTTPS protocols. It encapsulates the concept of acceptable status codes and content types, which determine the success or failure of a request.
  */
-@interface ZincHTTPRequestOperation : ZincNetworkOperation {
-@private
-    NSIndexSet *_acceptableStatusCodes;
-    NSSet *_acceptableContentTypes;
-    
-    NSData *_responseData;
-    NSInteger _totalBytesRead;
-    NSMutableData *_dataAccumulator;
-}
+@interface ZincHTTPRequestOperation : ZincURLConnectionOperation
 
-
-///----------------------------
-/// @name Getting Response Data
-///----------------------------
+///----------------------------------------------
+/// @name Getting HTTP URL Connection Information
+///----------------------------------------------
 
 /**
- The data received during the request. 
+ The last HTTP response received by the operation's connection.
  */
-@property (readonly, nonatomic, retain) NSData *responseData;
-
-/**
- The string representation of the response data.
- 
- @discussion This method uses the string encoding of the response, or if UTF-8 if not specified, to construct a string from the response data.
- */
-//@property (readonly, nonatomic, copy) NSString *responseString;
-
-///------------------------
-/// @name Accessing Streams
-///------------------------
-
-/**
- The output stream that is used to write data received until the request is finished.
- 
- @discussion By default, data is accumulated into a buffer that is stored into `responseData` upon completion of the request. When `outputStream` is set, the data will not be accumulated into an internal buffer, and as a result, the `responseData` property of the completed request will be `nil`.
- */
-@property (nonatomic, retain) NSOutputStream *outputStream;
-
+@property (readonly, nonatomic, strong) NSHTTPURLResponse *response;
 
 ///----------------------------------------------------------
 /// @name Managing And Checking For Acceptable HTTP Responses
 ///----------------------------------------------------------
-
-/**
- Returns an `NSIndexSet` object containing the ranges of acceptable HTTP status codes. When non-`nil`, the operation will set the `error` property to an error in `AFErrorDomain`. See http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
- 
- By default, this is the range 200 to 299, inclusive.
- */
-@property (nonatomic, retain) NSIndexSet *acceptableStatusCodes;
 
 /**
  A Boolean value that corresponds to whether the status code of the response is within the specified set of acceptable status codes. Returns `YES` if `acceptableStatusCodes` is `nil`.
@@ -82,16 +47,63 @@
 @property (readonly) BOOL hasAcceptableStatusCode;
 
 /**
+ A Boolean value that corresponds to whether the MIME type of the response is among the specified set of acceptable content types. Returns `YES` if `acceptableContentTypes` is `nil`.
+ */
+@property (readonly) BOOL hasAcceptableContentType;
+
+/** 
+ The callback dispatch queue on success. If `NULL` (default), the main queue is used.
+ */
+@property (nonatomic, assign) dispatch_queue_t successCallbackQueue;
+
+/** 
+ The callback dispatch queue on failure. If `NULL` (default), the main queue is used.
+ */
+@property (nonatomic, assign) dispatch_queue_t failureCallbackQueue;
+
+///------------------------------------------------------------
+/// @name Managing Acceptable HTTP Status Codes & Content Types
+///------------------------------------------------------------
+
+/**
+ Returns an `NSIndexSet` object containing the ranges of acceptable HTTP status codes. When non-`nil`, the operation will set the `error` property to an error in `AFErrorDomain`. See http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+ 
+ By default, this is the range 200 to 299, inclusive.
+ */
++ (NSIndexSet *)acceptableStatusCodes;
+
+/**
+ Adds status codes to the set of acceptable HTTP status codes returned by `+acceptableStatusCodes` in subsequent calls by this class and its descendants.
+ 
+ @param statusCodes The status codes to be added to the set of acceptable HTTP status codes
+ */
++ (void)addAcceptableStatusCodes:(NSIndexSet *)statusCodes;
+
+/**
  Returns an `NSSet` object containing the acceptable MIME types. When non-`nil`, the operation will set the `error` property to an error in `AFErrorDomain`. See http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.17 
  
  By default, this is `nil`.
  */
-@property (nonatomic, retain) NSSet *acceptableContentTypes;
++ (NSSet *)acceptableContentTypes;
 
 /**
- A Boolean value that corresponds to whether the MIME type of the response is among the specified set of acceptable content types. Returns `YES` if `acceptableContentTypes` is `nil`.
+ Adds content types to the set of acceptable MIME types returned by `+acceptableContentTypes` in subsequent calls by this class and its descendants.
+
+ @param contentTypes The content types to be added to the set of acceptable MIME types
  */
-@property (readonly) BOOL hasAcceptableContentType;
++ (void)addAcceptableContentTypes:(NSSet *)contentTypes;
+
+
+///-----------------------------------------------------
+/// @name Determining Whether A Request Can Be Processed
+///-----------------------------------------------------
+
+/**
+ A Boolean value determining whether or not the class can process the specified request. For example, `AFJSONRequestOperation` may check to make sure the content type was `application/json` or the URL path extension was `.json`.
+ 
+ @param urlRequest The request that is determined to be supported or not supported for this class.
+ */
++ (BOOL)canProcessRequest:(NSURLRequest *)urlRequest;
 
 ///-----------------------------------------------------------
 /// @name Setting Completion Block Success / Failure Callbacks
@@ -101,35 +113,21 @@
  Sets the `completionBlock` property with a block that executes either the specified success or failure block, depending on the state of the request on completion. If `error` returns a value, which can be caused by an unacceptable status code or content type, then `failure` is executed. Otherwise, `success` is executed.
  
  @param success The block to be executed on the completion of a successful request. This block has no return value and takes two arguments: the receiver operation and the object constructed from the response data of the request.
- @param failure The block to be executed on the completion of an unsuccessful request. This block has no return value and takes two arguments: the receiver operation and the error that occured during the request.
+ @param failure The block to be executed on the completion of an unsuccessful request. This block has no return value and takes two arguments: the receiver operation and the error that occurred during the request.
  
  @discussion This method should be overridden in subclasses in order to specify the response object passed into the success block.
  */
 - (void)setCompletionBlockWithSuccess:(void (^)(ZincHTTPRequestOperation *operation, id responseObject))success
                               failure:(void (^)(ZincHTTPRequestOperation *operation, NSError *error))failure;
 
-
-
-///---------------------------------
-/// @name Setting Progress Callbacks
-///---------------------------------
-
-/**
- Sets a callback to be called when an undetermined number of bytes have been uploaded to the server.
- 
- @param block A block object to be called when an undetermined number of bytes have been uploaded to the server. This block has no return value and takes three arguments: the number of bytes read since the last time the upload progress block was called, the total bytes read, and the total bytes expected to be read during the request, as initially determined by the expected content size of the `NSHTTPURLResponse` object. This block may be called multiple times.
- 
- @see setUploadProgressBlock
- */
-- (void)setDownloadProgressBlock:(void (^)(NSInteger bytesRead, NSInteger totalBytesRead, NSInteger totalBytesExpectedToRead))block;
-
-
 @end
 
+///----------------
+/// @name Functions
+///----------------
 
-typedef void (^ZincURLConnectionOperationProgressBlock)(NSInteger bytes, NSInteger totalBytes, NSInteger totalBytesExpected);
-
-
-
-
+/**
+ Returns a set of MIME types detected in an HTTP `Accept` or `Content-Type` header.
+ */
+extern NSSet * ZincContentTypesFromHTTPHeader(NSString *string);
 
