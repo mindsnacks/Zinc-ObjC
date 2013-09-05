5578764814ba460b727334682c88c5ab29eb4387
Andy Mroczkowski <andy@mrox.net>
2013-09-04 20:15:40 -0700

    Removing unnecessary ifdef

--- Zinc/Public/Zinc.h
+++ Zinc/Public/Zinc.h
@@ -6,9 +6,6 @@
 //  Copyright (c) 2012 MindSnacks. All rights reserved.
 //
 
-#ifndef _ZINC_
-#define _ZINC_
-
 #import <Zinc/ZincGlobals.h>
 #import <Zinc/ZincErrors.h>
 #import <Zinc/ZincRepo.h>
@@ -26,4 +23,3 @@
 #import <Zinc/ZincRepoMonitor.h>
 #import <Zinc/ZincBundleAvailabilityMonitor.h>
 
-#endif
