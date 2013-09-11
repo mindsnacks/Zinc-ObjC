a76b98b7ecb68e0d894d43feca4023a030be4d05
Andy Mroczkowski <andy@mrox.net>
2013-09-11 10:18:21 -0700

    Adding mock factory

---
+++ ZincTests/ZincMockFactory.h
@@ -0,0 +1,13 @@
+//
+//  ZincMockFactory.h
+//  Zinc-ObjC
+//
+//  Created by Andy Mroczkowski on 9/10/13.
+//  Copyright (c) 2013 MindSnacks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZincMockFactory : NSObject
+
+@end
