83aad8257fbd3bd3c1ffa95d1badec5a3709215d
Andy Mroczkowski <andy@mrox.net>
2013-09-11 21:44:09 -0700

    More Docs

--- Zinc/ZincActivityMonitor.h
+++ Zinc/ZincActivityMonitor.h
@@ -18,18 +18,53 @@
  `ZincActivityMonitor`
 
  This class is part of the *Zinc Public API*.
+ 
+ ## Usage Notes
 
+ - `startMonitoring` must be called to start the monitor
  */
 @interface ZincActivityMonitor : NSObject
 
+///-----------------
+/// @name Monitoring
+//------------------
+
+/**
+ @param refreshInterval set the auto refresh interval. Set to `0` to disable  auto refresh
+ */
 @property (nonatomic, assign) NSTimeInterval refreshInterval;
 
-@property (nonatomic, copy) ZincProgressBlock progressBlock;
-
+/**
+ Start monitoring
+ */
 - (void) startMonitoring;
+
+/**
+ Start monitoring
+ */
 - (void) stopMonitoring;
+
+/**
+ @return `YES` if current monitoring, `NO` otherwise
+ */
 @property (nonatomic, readonly, assign) BOOL isMonitoring;
 
+///---------------
+/// @name Progress
+//----------------
+
+/**
+ Set a block to be called when progress has been updated. The block will be
+ call for each individual item *and* for overall progress. For each individual
+ item, the `source` parameter of the block will be the `ZincActivityItem`
+ object. For overall progress, the `source` parameter will be the
+ `ZincActivityMonitor`.
+ */
+@property (nonatomic, copy) ZincProgressBlock progressBlock;
+
+/**
+ @return an array of all `ZincActivityItem` objects this monitor owns
+ */
 - (NSArray*) items;
 
 @end
@@ -43,8 +78,14 @@
  */
 @interface ZincActivityItem : ZincProgressItem
 
+/**
+ Reference to owning `ZincActivityMonitor`
+ */
 @property (nonatomic, readonly, weak) ZincActivityMonitor* monitor;
 
+/**
+ Reference to associated operation. May be nil.
+ */
 @property (nonatomic, readonly, strong) ZincOperation* operation;
 
 @end
