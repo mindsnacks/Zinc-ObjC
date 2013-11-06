9c2737015971adfbe5507a29f5da16c551e384b0
Andy Mroczkowski <andy@mrox.net>
2013-11-05 18:18:22 -0800

    Moving test to actual test target
    Novel, huh?

--- ZincDemo/AppDelegate.m
+++ ZincDemo/AppDelegate.m
@@ -30,47 +30,10 @@
     [super dealloc];
 }
 
-- (void) testBundleImageLoading
-{
-    // NOTE: this should be a unit test, but they don't execute in the same 
-    // evironment so I'm adding a little something here
-    
-    NSError* error = nil;
-    
-    NSString* dstDir = [ZincGetApplicationDocumentsDirectory() stringByAppendingPathComponent:@"testimages"];
-    [[NSFileManager defaultManager] removeItemAtPath:dstDir error:NULL];
-    [[NSFileManager defaultManager] createDirectoryAtPath:dstDir withIntermediateDirectories:YES attributes:nil error:&error];
-    NSAssert(error==nil, @"failed to create dir");
-    
-    for (NSString* file in [NSArray arrayWithObjects:@"sphalerite.jpg", @"sphalerite@2x.jpg", nil]) {
-        if (![[NSFileManager defaultManager] copyItemAtPath:[[NSBundle mainBundle] pathForResource:file ofType:nil] toPath:[dstDir stringByAppendingPathComponent:file] error:&error]) {
-            NSLog(@"error: %@", error);
-            abort();
-        }
-    }
-         
-    NSBundle* bundle = [NSBundle bundleWithPath:dstDir];
-    UIImage* image1 = [UIImage zinc_imageNamed:@"sphalerite.jpg" inBundle:bundle];
-    NSAssert(image1, @"image1 is nil");
-    NSLog(@"image1: %@", NSStringFromCGSize(image1.size));
-    
-    UIImage* image2 = [UIImage zinc_imageNamed:@"sphalerite@2x.jpg" inBundle:bundle];
-    NSAssert(image2, @"image1 is nil");
-    NSLog(@"image2: %@", NSStringFromCGSize(image2.size));
-
-    if ([UIScreen mainScreen].scale == 2.0f) {
-        NSAssert(image1.size.width == image2.size.width, @"retina wrong");
-        
-    } else {
-        NSAssert(image1.size.width*2 == image2.size.width, @"non-retina wrong");
-    }
-}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
-    
-    [self testBundleImageLoading];
     
     NSError* error = nil;
     
