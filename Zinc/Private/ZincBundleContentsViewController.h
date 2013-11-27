c35cd5fb37712ffe91cb7ecd2e03bd95eb78aceb
Andy Mroczkowski <andy@mrox.net>
2013-11-26 17:07:08 -0800

    Adding contents browser

---
+++ Zinc/Private/ZincBundleContentsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZincBundleContentsViewController.h
+//  Zinc-ObjC
+//
+//  Created by Andy Mroczkowski on 11/26/13.
+//  Copyright (c) 2013 MindSnacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZincBundleContentsViewController : UITableViewController
+
+@end
