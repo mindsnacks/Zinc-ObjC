32cd79154d5716e366111102d5d3657908899d86
Matthew Sun <msunmail@gmail.com>
2017-03-30 15:28:43 -0700

    tmp2

--- Zinc/Private/ZincContentBundleDeleteTask.m
+++ Zinc/Private/ZincContentBundleDeleteTask.m
@@ -33,11 +33,9 @@ static NSString * const kContentBundlePrefix = @"com.wonder.content";
 - (void)doMaintenance {
     NSLog(@"ContentBundleDelete started");
 
-    /*
     if ([self totalSizeOfContentBundles] < kContentBundleFlushLimitInMegabytes) {
         return;
     }
-     */
 
     NSMutableSet<NSURL *> *contentBundleURLs = [NSMutableSet new];
     NSMutableSet<NSString *> *contentBundleIDs = [NSMutableSet new];
