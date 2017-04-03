65d2d9c3820ec1771bd6a04ae90eaa8d2feb115f
Matthew Sun <msunmail@gmail.com>
2017-04-03 14:23:33 -0700

    tmp6

--- Zinc/Private/ZincRepo.m
+++ Zinc/Private/ZincRepo.m
@@ -34,8 +34,10 @@
 #define DOWNLOADS_DIR @"zinc/downloads"
 #define REPO_INDEX_FILE @"repo.json"
 
-static NSMutableDictionary* _ReposByURL;
+static const float kContentBundleFlushLimitInMegabytes = -1.f;
+static NSString * const kContentBundlePrefix = @"com.wonder.content";
 
+static NSMutableDictionary* _ReposByURL;
 
 NSString* const ZincRepoReachabilityChangedNotification = @"ZincRepoReachabilityChangedNotification";
 
@@ -136,7 +138,7 @@ NSString* const ZincRepoTaskNotificationTaskKey = @"task";
             }
 
             [repo queueGarbageCollectTask];
-            [repo queueContentBundleDeleteTask];
+            [repo deleteAllContentBundlesIfNeeded];
         }
     }
 
@@ -1045,10 +1047,224 @@ NSString* const ZincRepoTaskNotificationTaskKey = @"task";
     return [self.taskManager queueTaskForDescriptor:taskDesc];
 }
 
+/*
 - (ZincTask*) queueContentBundleDeleteTask
 {
     ZincTaskDescriptor* taskDesc = [ZincContentBundleDeleteTask taskDescriptorForResource:self.url];
     return [self.taskManager queueTaskForDescriptor:taskDesc];
+}
+ */
+
+- (NSString *)bundleNameForBundleURL:(NSURL *)bundleURL {
+    return [[bundleURL absoluteString] lastPathComponent];
+}
+
+- (NSURL *)urlForSearchPathDirectory:(NSSearchPathDirectory)searchPathDirectory {
+    NSURL *url = [NSFileManager.defaultManager URLsForDirectory:searchPathDirectory inDomains:NSUserDomainMask][0];
+    NSAssert(url != nil, @"url not found for NSSearchPathDirectory: %ld", (long)searchPathDirectory);
+    return url;
+}
+
+- (NSURL *)absoluteURLForPathRelativeToZincFolder:(NSString *)path isDirectory:(BOOL)isDirectory {
+    NSURL *docDirURL = [self urlForSearchPathDirectory:NSDocumentDirectory];
+    NSURL *zincDirURL = [docDirURL URLByAppendingPathComponent:@"zinc"];
+    NSURL *url = [zincDirURL URLByAppendingPathComponent:path
+                                             isDirectory:isDirectory];
+    NSParameterAssert(url);
+    return url;
+
+}
+
+- (NSURL *)bundlesFolderURL {
+    return [self absoluteURLForPathRelativeToZincFolder:@"bundles" isDirectory:YES];
+}
+
+- (void)forBundlesWithPrefix:(NSString *)prefix
+                performBlock:(void (^ _Nonnull)(NSURL * bundleURL))block {
+    NSParameterAssert(prefix);
+    NSParameterAssert(block);
+
+    NSURL *bundlesFolderURL = [self bundlesFolderURL];
+
+    NSError *error = nil;
+    NSArray<NSURL *> *bundleURLs = [NSFileManager.defaultManager contentsOfDirectoryAtURL:bundlesFolderURL
+                                                               includingPropertiesForKeys:nil
+                                                                                  options:0
+                                                                                    error:nil];
+    NSAssert(error == nil,
+             @"Error while getting bundle URLs: %@ \nfor bundle folder path:",
+             error.localizedDescription,
+             bundlesFolderURL.path);
+
+    for (NSURL *bundleURL in bundleURLs) {
+        NSString *bundleName = [self bundleNameForBundleURL:bundleURL];
+        if ([bundleName hasPrefix:prefix]) {
+            block(bundleURL);
+        }
+    }
+}
+
+- (unsigned long long int)sizeOfFolderInBytesWithPath:(NSString *)folderPath {
+    NSError *error = nil;
+    NSArray *filesArray = [NSFileManager.defaultManager subpathsOfDirectoryAtPath:folderPath error:nil];
+    NSAssert(error == nil,
+             @"Error while getting folder subpaths: %@ \nfor folder path:",
+             error.localizedDescription,
+             folderPath);
+
+    NSEnumerator *filesEnumerator = [filesArray objectEnumerator];
+    NSString *fileName;
+    unsigned long long int totalSize = 0;
+
+    while (fileName = [filesEnumerator nextObject]) {
+        NSString *filePath = [folderPath stringByAppendingPathComponent:fileName];
+        NSDictionary *fileDictionary = [NSFileManager.defaultManager attributesOfItemAtPath:filePath error:nil];
+        totalSize += [fileDictionary fileSize];
+    }
+
+    return totalSize;
+}
+
+- (float)megabytesForBytes:(unsigned long long)bytes {
+    return bytes / 1024.f / 1024.f;
+}
+
+- (unsigned long long)totalSizeInBytesOfBundlesWithPrefix:(NSString *)prefix {
+    __block unsigned long long size = 0;
+
+    [self forBundlesWithPrefix:prefix performBlock:^(NSURL *bundleURL) {
+        size += [self sizeOfFolderInBytesWithPath:bundleURL.path];
+    }];
+
+    return size;
+}
+
+- (float)totalSizeInMegabytesOfBundlesWithPrefix:(NSString *)prefix {
+    unsigned long long bytes = [self totalSizeInBytesOfBundlesWithPrefix:prefix];
+    return [self megabytesForBytes:bytes];
+}
+
+- (float)totalSizeOfContentBundles {
+    return [self totalSizeInMegabytesOfBundlesWithPrefix:kContentBundlePrefix];
+}
+
+// Example content bundle name: @"com.wonder.content4.sat-BELLY-0355-1"
+// Example content bundle id: @"com.wonder.content4.sat-BELLY-0355"
+- (NSString *)contentBundleIDForContentBundleName:(NSString *)bundleName {
+    NSString *contentBundleRegexPattern = @"\\A(com\\.wonder\\.content\\d*\\..*)-(.*)\\Z";
+
+    NSError *error;
+    NSRegularExpression *contentBundleRegex = [NSRegularExpression regularExpressionWithPattern:contentBundleRegexPattern
+                                                                                        options:NSRegularExpressionAnchorsMatchLines
+                                                                                          error:&error];
+    NSAssert(error == nil, @"error when creating content bundle regex: %@", error);
+
+    NSTextCheckingResult *match = [contentBundleRegex firstMatchInString:bundleName
+                                                                 options:NSMatchingAnchored
+                                                                   range:NSMakeRange(0, bundleName.length)];
+    NSParameterAssert(match);
+
+    NSString *bundleID = [bundleName substringWithRange:[match rangeAtIndex:1]]; // e.g. "com.wonder.content4.sat-BELLY-0355"
+    NSString *zincVersionString = [bundleName substringWithRange:[match rangeAtIndex:2]]; // e.g. "1"
+    NSParameterAssert(bundleID);
+    NSParameterAssert(zincVersionString);
+
+    return bundleID;
+}
+
+- (void) xderegisterBundle:(NSURL*)bundleResource completion:(dispatch_block_t)completion
+{
+    [self postNotification:ZincRepoBundleWillDeleteNotification bundleID:[bundleResource zincBundleID]];
+    @synchronized(self.index) {
+        [self.index removeBundle:bundleResource];
+    }
+    ZincTask* saveTask = [self queueIndexSaveTask];
+    if (completion != nil) {
+        ZincTaskRef* taskRef = [[ZincTaskRef alloc] init];
+        [taskRef addDependency:saveTask];
+        taskRef.completionBlock = completion;
+        [self.taskManager addOperation:taskRef];
+    }
+}
+
+- (NSString *)manifestNameForBundleName:(NSString *)bundleName {
+    return [bundleName stringByAppendingString:@".json"];
+}
+
+- (NSURL *)manifestsFolderURL {
+    return [self absoluteURLForPathRelativeToZincFolder:@"manifests" isDirectory:YES];
+}
+
+- (NSURL *)manifestURLForManifestName:(NSString *)manifestName {
+    return [[self manifestsFolderURL] URLByAppendingPathComponent:manifestName
+                                                      isDirectory:NO];
+}
+
+- (BOOL)deleteFileAtURL:(NSURL *)url {
+    NSError *error;
+    [NSFileManager.defaultManager removeItemAtURL:url error:&error];
+    if (error) {
+        NSLog(@"Error while deleting file at url: %@ \nfor file path: %@",
+              error.localizedDescription,
+              url.path);
+    }
+
+    return !error;
+}
+
+- (void)purgeBundlesWithIDS:(NSSet<NSString *> *)bundleIDs bundleURLs:(NSSet<NSURL *> *)bundleURLs {
+    @synchronized(self.index) {
+        for (NSString *bundleID in bundleIDs) {
+            [self postNotification:ZincRepoBundleWillDeleteNotification bundleID:bundleID];
+            [self.index purgeBundleWithID:bundleID];
+        }
+    }
+
+    ZincTaskRef* taskRef = [[ZincTaskRef alloc] init];
+    taskRef.completionBlock = ^{
+        for (NSURL *bundleURL in bundleURLs) {
+            NSString *bundleName = [self bundleNameForBundleURL:bundleURL];
+            NSString *manifestName = [self manifestNameForBundleName:bundleName];
+            NSURL *manifestURL = [self manifestURLForManifestName:manifestName];
+
+            if ([self deleteFileAtURL:manifestURL]) {
+                [self deleteFileAtURL:bundleURL];
+            } else {
+                NSLog(@"Failed to delete a manifest, will not delete the related bundle: %@",
+                      bundleURL);
+            }
+        }
+
+        [NSNotificationCenter.defaultCenter postNotificationName:kZincAllContentBundlesWereDeleted
+                                                          object:self
+                                                        userInfo:nil];
+    };
+    [taskRef addDependency:[self queueIndexSaveTask]];
+    [self.taskManager addOperation:taskRef];
+}
+
+- (void) deleteAllContentBundlesIfNeeded {
+    float totalSizeOfContentBundles = [self totalSizeOfContentBundles];
+    NSString *message = [NSString stringWithFormat:@"Total size of content bundles: %f", totalSizeOfContentBundles];
+
+    if (totalSizeOfContentBundles < kContentBundleFlushLimitInMegabytes) {
+        NSLog(@"%@, will not clear", message);
+        return;
+    }
+    NSLog(@"%@, will clear", message);
+
+    NSMutableSet<NSURL *> *contentBundleURLs = [NSMutableSet new];
+    NSMutableSet<NSString *> *contentBundleIDs = [NSMutableSet new];
+    [self forBundlesWithPrefix:kContentBundlePrefix performBlock:^(NSURL *bundleURL) {
+        NSString *bundleName = [self bundleNameForBundleURL:bundleURL];
+        NSString *bundleID = [self contentBundleIDForContentBundleName:bundleName];
+
+        [contentBundleURLs addObject:bundleURL];
+        [contentBundleIDs addObject:bundleID];
+    }];
+
+    [self purgeBundlesWithIDS:contentBundleIDs
+                   bundleURLs:contentBundleURLs];
 }
 
 - (ZincTask*) queueCleanSymlinksTask
